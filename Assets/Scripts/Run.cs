0a9776a73fd44e1027fea3ba340121bcd32e34fc
Kyle James <kyleajames102@gmail.com>
2021-06-18 20:30:12 +0100

    Started creating initial running mechanic and score counter.

---
+++ Assets/Scripts/Run.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Run : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI scoreText;
+    public Animation runAnim;
+
+    private float score;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            score += 0.1f;
+            runAnim.Play();
+        }
+
+        scoreText.text = "Distance: " + score.ToString("F1") + "M";
+    }
+}
